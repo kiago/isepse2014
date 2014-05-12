bf4f50451e45536b2292744d7ec81db10b07c487
tferrand <tferrand@isep.fr>
2014-05-12 14:42:58 +0200

    added code to browse a folder files + extract the name of the episode without the extention + extract season number + extract episode number

--- thomas.ferrandini/Projet/subtitle.sh
+++ thomas.ferrandini/Projet/subtitle.sh
@@ -6,9 +6,38 @@
 
 #curl -s http://www.opensubtitles.org/fr/search/imdbid-3361956/sublanguageid-fre/moviename-bates%20motel | sed -n '/<a class=\"bnone".*/,/<\/a>/p' > test.txt
 
+#download link
 #curl -o test.zip http://dl.opensubtitles.org/fr/download/sub/5089217
 
 for fichier in ~/Movies/test/*
 do
-   	basename $fichier.##* > test.txt
+	#TVshow complete name + ep + season
+   	fichier=${fichier##*/};
+   	fichier=${fichier%.*};
+   	echo $fichier
+
+   	#episode season + episode
+   	episode=${fichier##*.};
+   	echo $episode
+
+   	#season number
+   	season=$(echo "$episode" | cut -c2-3);
+   	echo $season;
+
+   	#episode number
+   	ep=$(echo "$episode" | cut -c5-6);
+   	echo $ep;
+
+
+   	#episode=${fichier}
+   	#echo “filename: ${fichier%.*}”
+	#echo “extension: ${fichier##*.}”
+
+	curl -s "http://www.opensubtitles.org/fr/search/sublanguageid-fre/season-${season}/episode-${ep}/moviename-${fichier}" > test.txt
+
 done
+
+#curl -s "http://www.opensubtitles.org/fr/search2/sublanguageid-fre/moviename-devious.maids.s02e03" | sed -n '/<a href=\"\/fr\/subtitleserve\/sub\/5649116\".*/,/<\/a>/p' > test.txt
+
+#New url : we need to specify the episode and season number 
+#http://www.opensubtitles.org/fr/search/sublanguageid-fre/season-2/episode-3/moviename-devious.maids.s02e02
