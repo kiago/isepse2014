e50ed5e055651ac6327d268604795275210faab8
tferrand <tferrand@isep.fr>
2014-05-12 10:24:58 +0200

    add questions

--- thomas.ferrandini/Projet/subtitle.sh
+++ thomas.ferrandini/Projet/subtitle.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-curl -s http://www.opensubtitles.org/fr/search2/sublanguageid-fre/moviename-bates.motel.s01e02 | sed -n '/<a href=\"/fr/subtitleserve/sub/.*/,/<\/a>/p' > test.txt
+#curl -s http://www.opensubtitles.org/fr/search2/sublanguageid-fre/moviename-bates.motel.s01e02 | sed -n '/<a href=\"/fr/subtitleserve/sub/.*/,/<\/a>/p' > test.txt
 
-curl -s http://www.opensubtitles.org/fr/search/imdbid-3361956/sublanguageid-fre/moviename-bates%20motel | sed -n '/<a href=\"\/fr\/subtitleserve\/sub\/5580050\".*/,/<\/a>/p' > test.txt
+#curl -s http://www.opensubtitles.org/fr/search/imdbid-3361956/sublanguageid-fre/moviename-bates%20motel | sed -n '/<a href=\"\/fr\/subtitleserve\/sub\/5580050\".*/,/<\/a>/p' > test.txt
 
-curl -s http://www.opensubtitles.org/fr/search/imdbid-3361956/sublanguageid-fre/moviename-bates%20motel | sed -n '/<a class=\"bnone".*/,/<\/a>/p' > test.txt
+#curl -s http://www.opensubtitles.org/fr/search/imdbid-3361956/sublanguageid-fre/moviename-bates%20motel | sed -n '/<a class=\"bnone".*/,/<\/a>/p' > test.txt
 
-curl -o test.zip http://dl.opensubtitles.org/fr/download/sub/5089217
+#curl -o test.zip http://dl.opensubtitles.org/fr/download/sub/5089217
+
+for fichier in ~/Movies/test/*
+do
+   	basename $fichier.##* > test.txt
+done
