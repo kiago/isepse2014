7a773ca2754e8a80bfd8dd9c85f42991164904df
tferrand <tferrand@isep.fr>
2014-05-16 17:23:51 +0200

    added help function + added possibility to select the subtitle language in parameter of the script (french by default) + gived the choice of the folder where the videos are to the user

--- thomas.ferrandini/Projet/subtitle.sh
+++ thomas.ferrandini/Projet/subtitle.sh
@@ -9,48 +9,84 @@
 #download link
 #curl -o test.zip http://dl.opensubtitles.org/fr/download/sub/5089217
 
-for fichier in ~/Movies/test/*
-do
-	#TVshow complete name + ep + season
-	fichier=${fichier##*/};
-	fichier=${fichier%.*};
-	echo $fichier
+# episode season + episode
+# episode=${fichier##*.};
+# echo $episode
 
-	#episode season + episode
-	episode=${fichier##*.};
-	echo $episode
+# #season number
+# season=$(echo "$episode" | cut -c2-3);
+# echo $season;
 
-	#season number
-	season=$(echo "$episode" | cut -c2-3);
-	echo $season;
+# #episode number
+# ep=$(echo "$episode" | cut -c5-6);
+# echo $ep;
 
-	#episode number
-	ep=$(echo "$episode" | cut -c5-6);
-	echo $ep;
+# #episode=${fichier}
+# #echo “filename: ${fichier%.*}”
+# #echo “extension: ${fichier##*.}”
 
-	#episode=${fichier}
-	#echo “filename: ${fichier%.*}”
-	#echo “extension: ${fichier##*.}”
+##This url requires to extract season number and episode 
+# #page=$(curl -s "http://www.opensubtitles.org/fr/search/sublanguageid-${lang}/season-${season}/episode-${ep}/moviename-${fichier}");
 
-	page=$(curl -s "http://www.opensubtitles.org/fr/search/sublanguageid-fre/season-${season}/episode-${ep}/moviename-${fichier}");
+function help_function {
+	echo -e "This application download all subtitles for the videos in a folder \n"
+	echo -e "Utilisation : ./subtitle.sh [-OPTS] [MOTS CLEFS...] \n"
+}
 
-	sub_link=$(echo "$page" | grep -o '<a href="/fr/subtitleserve/sub/[^"]*"' | sed 's/<a href="\/fr\/subtitleserve\///;s/"$//');
+function ask_path {
+	read -e -p "Enter the path where your videos are : " FILEPATH
+	eval FILEPATH=$FILEPATH
+	cd $FILEPATH
+	#echo $PWD
+}
 
-	echo $sub_link; 
-	curl -o ~/Movies/test/${fichier}.zip "http://dl.opensubtitles.org/fr/download/${sub_link}";
+function download_subtitles {
+	echo $lang
 
-	#Extract .srt from zip and delete .zip
-   	unzip ~/Movies/test/${fichier}.zip "*.srt" -d ~/Movies/test/${fichier} && rm ~/Movies/test/${fichier}.zip;
+	ask_path
 
-   	#move the srt file next to the videos
-   	mv ~/Movies/test/${fichier}/*.srt ~/Movies/test/${fichier}.srt;
+	for fichier in $PWD/*.avi
+	do
+		#TVshow complete name + ep + season
+		fichier=${fichier##*/};
+		fichier=${fichier%.*};
+		echo $fichier
 
-   	#delete the old folder containing the subtitles
-   	rm -r ~/Movies/test/${fichier};
 
-done
+		#This link doesn't need to extract episode number and season
+		page=$(curl -s "http://www.opensubtitles.org/fr/search/sublanguageid-${lang}/moviename-${fichier}");
 
-#curl -s "http://www.opensubtitles.org/fr/search2/sublanguageid-fre/moviename-devious.maids.s02e03" | sed -n '/<a href=\"\/fr\/subtitleserve\/sub\/5649116\".*/,/<\/a>/p' > test.txt
+		sub_link=$(echo "$page" | grep -o '<a href="/fr/subtitleserve/sub/[^"]*"' | sed 's/<a href="\/fr\/subtitleserve\///;s/"$//');
 
-#New url : we need to specify the episode and season number 
-#http://www.opensubtitles.org/fr/search/sublanguageid-fre/season-2/episode-3/moviename-devious.maids.s02e02
+		echo $sub_link; 
+		curl -o $PWD/${fichier}.zip "http://dl.opensubtitles.org/fr/download/${sub_link}";
+
+		#Extract .srt from zip and delete .zip
+	   	unzip $PWD/${fichier}.zip "*.srt" -d $PWD/${fichier} && rm $PWD/${fichier}.zip;
+
+	   	#move the srt file next to the videos
+	   	mv $PWD/${fichier}/*.srt $PWD/${fichier}.srt;
+
+	   	#delete the old folder containing the subtitles
+	   	rm -r $PWD/${fichier};
+
+	done
+}
+
+#Default language
+lang="fre"
+
+# verify if script has a parameter
+if [[ $# != 0 ]] ; then
+	# We show the help if required
+	if [[ $# == 1 ]] ; then 
+		if [[ $1 == '-h' || $1 == '--help' ]] ; then
+		    help_function
+		else
+			lang=$(echo "$@" | cut -c1-3);
+			download_subtitles
+		fi
+	else download_subtitles
+	fi
+else download_subtitles
+fi
