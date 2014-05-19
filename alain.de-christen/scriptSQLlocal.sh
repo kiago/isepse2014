64b4eed52164acc7ada5bc916bf9efbf393f2763
Alain de Christen <dev.alain@dechristen.fr>
2014-05-19 09:16:33 +0200

    Adding keynote PPT

--- alain.de-christen/scriptSQLlocal.sh
+++ alain.de-christen/scriptSQLlocal.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 # Base de données
  user="root"
  password="root"
@@ -11,13 +9,10 @@
  backup_path="/Users/juniorisep/Documents/gitrepository/isepse2014/alain.de-christen/03-LocalSyncFolder/SQL-bashproject/"
  date=$(date +"%d-%b-%Y")
 
-# Permissions de fichier
+# Permissions d'écriture et de lecture pour le propriétaire uniquement
  umask 177
 
 # Dump de la base en un fichier SQL
  $mysqldump --user=$user --password=$password --host=$host $db_name > $backup_path/$db_name-$date.sql
-
-# Supprimer les sauvegardes de plus de 30 jours
-# find $backup_path/* -mtime +30 -exec rm {} \;
 
 echo "La sauvegarde SQL a eu lieu avec succès".
