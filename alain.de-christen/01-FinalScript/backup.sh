199f7f96d1dee791495508110ad0f742b2de7c75
Alain de Christen <dev.alain@dechristen.fr>
2014-06-01 12:36:39 +0200

    Crontab command

--- alain.de-christen/01-FinalScript/backup.sh
+++ alain.de-christen/01-FinalScript/backup.sh
@@ -25,17 +25,11 @@ umask 177 # Owner only has access in reading and writing
 rsync -arv $source $allbackups/backup-$date
 
 # SQL backup script
-#mysqldump -u $user -p waJPtbG2 --databases $(
-#  mysql -u $user -p waJPtbG2 -N $dbname \
-#    -e "SELECT DISTINCT(TABLE_SCHEMA)
-#        FROM tables
-#        WHERE TABLE_SCHEMA LIKE 'isepa_%'"
-#) > $allbackups/backup-$date/$sqldumpname.sql
-
-mysqldump -u $user -p waJPtbG2 --databases $(mysql -u $user -p waJPtbG2 -N $dbname -Bse "show tables like 'aaa'") > $allbackups/backup-$date/$sqldumpname.sql
+# dump of the database including tables with prefix "aaa" only
+# mysqldump -u $user -p waJPtbG2 --databases $(mysql -u $user -p waJPtbG2 -N $dbname -Bse "show tables like 'aaa'") > $allbackups/backup-$date/$sqldumpname.sql
 
 #basic dump of the database including all tables
-#mysqldump --user=$user  --password=$password --host=$host $dbname > $allbackups/backup-$date/$sqldumpname.sql
+mysqldump --user=$user  --password=$password  --host=$host $dbname > $allbackups/backup-$date/$sqldumpname.sql
 
 # Save the logs to a external file when executing the script
 backup.sh &>logBackup.txt
