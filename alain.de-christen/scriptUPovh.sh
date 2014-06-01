1dac4d489da09ab4da55f51de29208ba8f80409a
Alain de Christen <dev.alain@dechristen.fr>
2014-06-01 22:58:30 +0200

    Delete/move some files

--- alain.de-christen/scriptUPovh.sh
+++
@@ -1,2 +0,0 @@
-rsync -arv /Users/juniorisep/Documents/gitrepository/isepse2014/alain.de-christen/"01-FinalScript"/ orthoeur@ftp.ortho-europe78.fr:~/www/isep/
-echo "Les scripts ont été envoyés vers le serveur OVH."
