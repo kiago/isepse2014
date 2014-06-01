2c30c7719e869ebef4e9031e54257086e951e558
Alain de Christen <dev.alain@dechristen.fr>
2014-06-01 11:37:24 +0200

    Reorganizing folder and server

--- alain.de-christen/scriptDLovh.sh
+++ alain.de-christen/scriptDLovh.sh
@@ -1,2 +1,2 @@
-rsync -arv orthoeur@ftp.ortho-europe78.fr:~/www/isep/  /Users/juniorisep/Documents/gitrepository/isepse2014/alain.de-christen/"04-OVHScripts"/
+rsync -arv orthoeur@ftp.ortho-europe78.fr:~/www/isep/  /Users/juniorisep/Documents/gitrepository/isepse2014/alain.de-christen/"01-FinalScript"/
 echo "Les scripts sur le serveur OVH ont été rapatriés."
