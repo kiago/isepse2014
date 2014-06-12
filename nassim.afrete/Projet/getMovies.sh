a9c7f1e6633992f1951992ed29582fa0055bc0d8
afrete nassim <nass_5902@hotmail.fr>
2014-06-12 10:03:49 +0200

    update script to get Movies

--- nassim.afrete/Projet/getMovies.sh
+++ nassim.afrete/Projet/getMovies.sh
@@ -11,9 +11,8 @@ tr -d '\n' < allMovies.html | grep -o -E '<h3><a class="fs12 bold" href="([^"#]+
 echo -e  "\nFin de Récupération des films"
 echo "Récupération des descriptions..."
 
-echo '<!DOCTYPE HTML><html lang="fr"><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><meta name="viewport" content="width=device-width, initial-scale=1.0" /><title>Films du box-office</title>
-<style type="text/css">body {text-align: center; padding-left: 100px; padding-right: 100px; padding-top: 20px; font-family: "Century Gothic",arial,serif;}div {border-bottom: 1px solid #E3E3E3;}div p:first-child {font-size: 20px;}div p:first-child + p {padding-left: 100px; padding-right: 100px;}h1 {text-align: center;font-size: 30px;}img {width: 280px;}</style>
-</head><body><h1>Les films du Box-Office : Projet IOSA</h1>' > boxOffice.html
+echo '<!DOCTYPE HTML><html lang="fr"><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><meta name="viewport" content="width=device-width, initial-scale=1.0" /><title>Films du box-office</title><style type="text/css">img {width: 280px;}</style>
+</head><body><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td><table align="center" width="800px" cellspacing="0" cellpadding="0" border="0" style="background-color:#2c3e50;color:#ecf0f1;font-family:'Century Gothic',arial,serif;margin:0px auto !important;"><tr><td style="background-color: rgb(231, 76, 60); height: 110px; width: 100%; vertical-align: middle; text-align: center;"><h1 style="color:#FFFFFF;font-weight: lighter;text-align: center;font-size: 30px;">Les films du Box-Office : Projet IOSA</h1></td></tr>' > boxOffice.html
 
 for movie in $(cat movies.html)
 do
@@ -24,13 +23,12 @@ title=`tr -d '\n' < pageMovie.html | grep -o -E '<meta property="og:title" conte
 description=`tr -d '\n' < pageMovie.html | grep -o -E '<meta property="og:description" content="([^"]+)" />' | sed 's/^.*content="\([^"]*\)".*$/\1/' | sed "s/&#39;/'/g" | sed 's/&quot;/"/g'`
 image=`tr -d '\n' < pageMovie.html | grep -o -E '<meta property="og:image" content="([^"]+)" />' | sed 's/^.*content="\([^"]*\)".*$/\1/'`
 
-echo "<div><p>$title</p>" >> boxOffice.html
-echo "<p>$description</p>" >> boxOffice.html
-echo "<p><img src=\"$image\" alt=\"image\" /></div>" >> boxOffice.html
+echo "<tr><td style=\"vertical-align: top\"><table width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\"><tr><td style=\"height:40px;\"></td></tr><tr><td style=\"width:20px;\"></td><td style=\"with:200px;\"><img src=\"$image\" alt=\"image\" style=\"display:block;border:0px;\" /></td><td style=\"width:20px;\"></td><td style=\"vertical-align: top;\"><div style=\"font-size: 34px; height: 102px;\">$title</div><div style=\"height: 210px;\">$description</div></td><td style=\"width:20px;\"></td></tr></table></td></tr>" >> boxOffice.html
+
 rm pageMovie.html
 
 done
 
-echo "</body></html>" >> boxOffice.html
+echo "</table></td></tr></table></body></html>" >> boxOffice.html
 
-mailx -a "Content-Type: text/html" -s "Films du Box-office : Projet IOSA" nass_5902@hotmail.fr daher860@hotmail.com baptcheg@gmail.com abtout.samy@gmail.com < boxOffice.html
+mailx -a "Content-Type: text/html" -s "Films du Box-office : Projet IOSA" nass_5902@hotmail.fr < boxOffice.html
